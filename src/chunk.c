381e89dbaf1c08e06df5d3ecba263de9f027e416
ZachElkins <zach.elkins@gmil.com>
2024-02-28 21:44:51 -0800

    run-length encoding line numbers

--- src/chunk.c
+++ src/chunk.c
@@ -8,6 +8,8 @@ void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lineCount = 0;
+    chunk->lineCapacity = 0;
     chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
@@ -18,18 +20,31 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code,
                                  oldCapacity, chunk->capacity);
-        chunk->lines = GROW_ARRAY(int, chunk->lines,
-                                  oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
-    chunk->lines[chunk->count] = line;
     chunk->count++;
+
+    if (chunk->lineCount > 0 &&
+        chunk->lines[chunk->lineCount-1].line == line) {
+        return;
+    }
+
+    if (chunk->lineCapacity < chunk->lineCount + 1) {
+        int oldCapacity = chunk->lineCapacity;
+        chunk->lineCapacity = GROW_CAPACITY(oldCapacity);
+        chunk->lines = GROW_ARRAY(LineStart, chunk->lines,
+                                  oldCapacity, chunk->lineCapacity);
+    }
+
+    LineStart* lineStart = &chunk->lines[chunk->lineCount++];
+    lineStart->offset = chunk->count - 1;
+    lineStart->line = line;
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-    FREE_ARRAY(int, chunk->lines, chunk->capacity);
+    FREE_ARRAY(LineStart, chunk->lines, chunk->lineCapacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
@@ -39,4 +54,22 @@ int addConstant(Chunk* chunk, Value value) {
     writeValueArray(&chunk->constants, value);
     pop();
     return chunk->constants.count - 1;
+}
+
+int getLine(Chunk* chunk, int instruction) {
+    int start = 0;
+    int end = chunk->lineCount - 1;
+
+    for (;;) {
+        int mid = (start + end) / 2;
+        LineStart* line = &chunk->lines[mid];
+        if (instruction < line->offset) {
+            end = mid - 1;
+        } else if (mid == chunk->lineCount - 1 ||
+                instruction < chunk->lines[mid + 1].offset) {
+            return line->line;
+        } else {
+            start = mid + 1;
+        }
+    }
 }
